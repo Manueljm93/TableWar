941d3e237e59297f4c6aaacabcffb01ac25445e7
Manueljm93 <manueljmpereira93@gmail.com>
2020-11-07 14:41:15 -0300

    TableWar Windows
    Preset Windows

---
+++ src/TableWar.java
@@ -0,0 +1,37 @@
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class TableWar extends Canvas {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final int ANCHO = 800;
+	private static final int ALTO = 600;
+	
+	private static final String NOMBRE = "TotalWar";
+	
+	private static JFrame ventana;
+	
+	private TableWar()
+	{
+			setPreferredSize(new Dimension(ANCHO, ALTO));
+			ventana = new JFrame(NOMBRE);
+			ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			ventana.setResizable(false);
+			ventana.setLayout(new BorderLayout());
+			ventana.add(this,BorderLayout.CENTER);
+			ventana.pack();
+			ventana.setLocationRelativeTo(null);
+			ventana.setVisible(true);			
+	}
+
+	public static void main(String[] args) {
+	
+		TableWar tw = new TableWar();
+		
+	}
+
+}
